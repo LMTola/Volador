7995c80bd944d1495d316813fc012c61565e913f
ltola <luchiomick@gmail.com>
2021-10-07 12:52:08 -0300

    Agregar archivos de proyecto.

---
+++ Volador/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Volador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TorreDeControl torredecontrol = new TorreDeControl();
+
+            Pato pato = new Pato();
+            Boing747 boing747 = new Boing747();
+            Superman superman = new Superman();
+
+            torredecontrol.agregarVolador(pato);
+            torredecontrol.agregarVolador(boing747);
+            torredecontrol.agregarVolador(superman);
+
+            torredecontrol.vuelenTodos();
+
+        }
+    }
+}
---
+++ Volador/TorreDeControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volador
+{
+    class TorreDeControl
+    {
+        
+        List<Volador> Voladores = new List<Volador>();
+        public void agregarVolador(Volador unVolable)
+        {
+            Voladores.Add(unVolable);
+        }
+
+        public void vuelenTodos()
+        {
+            foreach (Volador c in Voladores)
+            {
+                c.Volar();
+            }
+        }
+
+
+    }
+}
